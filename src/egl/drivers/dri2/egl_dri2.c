cf1902f90ce5d3ccdae4ce64bdd1ea367de91fd5
Christian Gmeiner <cgmeiner@igalia.com>
2025-02-28 22:18:25 +0100

    egl_dri2: Add support for OHOS
    This is enough to make kopper happy.
    
    Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>

--- src/egl/drivers/dri2/egl_dri2.c
+++ src/egl/drivers/dri2/egl_dri2.c
@@ -913,6 +913,9 @@ dri2_initialize(_EGLDisplay *disp)
    case _EGL_PLATFORM_ANDROID:
       ret = dri2_initialize_android(disp);
       break;
+   case _EGL_PLATFORM_OHOS:
+      ret = dri2_initialize_ohos(disp);
+      break;
    default:
       unreachable("Callers ensure we cannot get here.");
       return EGL_FALSE;
